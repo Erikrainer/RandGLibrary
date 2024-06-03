5ebb73a640fc12921ff5dd4560effd43599aee5a
Erik <rainer.erik70@gmail.com>
2024-06-03 10:02:34 -0700

    vite config changed to see if fix the error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: 'https://randglibrary.onrender.com/',
         changeOrigin: true,
-        secure: false,
+        secure: true,
       }
     }
   }
